9b116db122b0ae8044e7474ab62e3c4ce1162b5e
narickson <mnarickson@gmail.com>
2020-02-13 13:44:20 +0300

    Update SecurityChecks.js
    Changed js code

--- www/SecurityChecks.js
+++ www/SecurityChecks.js
@@ -1,5 +1,5 @@
 var exec = require('cordova/exec');
 
-module.exports.SecurityPassFn = function(arg0, success, error) {
-    exec(success, error, 'SecurityChecks', 'SecurityPassFn', [arg0]);
+module.exports.validate = function(arg0, success, error) {
+    exec(success, error, 'SecurityChecks', 'validate', [arg0]);
 };
