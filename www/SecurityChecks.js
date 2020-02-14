40c5d78abc8223e45d2f78712543b793a4220332
narickson <mnarickson@gmail.com>
2020-02-14 14:54:13 +0300

    Update SecurityChecks.js
    Updated js code for the java functions

--- www/SecurityChecks.js
+++ www/SecurityChecks.js
@@ -1,5 +1,13 @@
 var exec = require('cordova/exec');
 
-module.exports.validate = function(arg0, success, error) {
-    exec(success, error, 'SecurityChecks', 'validate', [arg0]);
+module.exports.checkEmulator = function(arg0, success, error) {
+    exec(success, error, 'SecurityChecks', 'checkEmulator', [arg0]);
+};
+
+module.exports.checkDebuggable = function(arg0, success, error) {
+    exec(success, error, 'SecurityChecks', 'checkDebuggable', [arg0]);
+};
+
+module.exports.checkDownloadSource = function(arg0, success, error) {
+    exec(success, error, 'SecurityChecks', 'checkDownloadSource', [arg0]);
 };
